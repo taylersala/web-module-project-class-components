39e2ab1d0c0cc3c12ed35a3896c4288af162a557
taylersala <taylersala@gmail.com>
2023-07-02 13:06:23 -0700

    working

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,48 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: true
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+  }
+}
   render() {
+    const { todos } = this.state;
+    //console.log(todos)
     return (
       <div>
-        Todo App
+       <h1>Todos</h1>
+       <ul>
+        {
+          todos.map(todo => {
+            return (<li>{todo.task} {todo.completed?<span>- completed</span> : <span></span> }</li>)
+          })
+        }
+      </ul>
+      
+      <form>
+        <input>
+        <button>Add</button>
+        </input>
+      </form>
+
+      <button>Clear</button>
+      
+      
       </div>
+
     )
   }
 }
